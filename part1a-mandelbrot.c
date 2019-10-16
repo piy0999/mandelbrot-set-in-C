6659276ec578d8b03be2ce73d49bd415c2ef3864
piyushjha <piyushjha0999@gmail.com>
2019-10-16 21:05:36 +0800

    part1a working

--- part1a-mandelbrot.c
+++ part1a-mandelbrot.c
@@ -22,7 +22,7 @@ typedef struct message {
 int main( int argc, char* args[] )
 {   
     if (argc != 2) {
-        printf("Usage: ./1amandelbrot [No. of Child Processes(int)]");
+        printf("Usage: ./1amandelbrot [number of child processes]");
         return 0;
     }
     char *a = args[1];
@@ -33,7 +33,6 @@ int main( int argc, char* args[] )
 	clock_gettime(CLOCK_MONOTONIC, &start_time);
 	
 	//data structure to store the start and end times of the computation
-	struct timespec start_compute, end_compute;
 	
 	//generate mandelbrot image and store each pixel for later display
 	//each pixel is represented as a value in the range of [0,1]
@@ -48,19 +47,23 @@ int main( int argc, char* args[] )
 	}
 	//compute the mandelbrot image
 	//keep track of the execution time - we are going to parallelize this part
-	printf("Start the computation ...\n");
-	clock_gettime(CLOCK_MONOTONIC, &start_compute);
+	//printf("Start the computation ...\n");
 	float difftime;
     int count = 0;
     int rows = (IMAGE_HEIGHT / num_child);
     int vert = 0;
     int pfd[2];
-    pipe(pfd); 
+    int resp = pipe(pfd); 
+    if (resp == 0){
+        printf("start collecting image lines\n"); //As Pipe was successfully created
+    }
     while (count < num_child){
         pid_t pid = fork();
             if (pid == 0) { 
+            struct timespec start_compute, end_compute;
+            clock_gettime(CLOCK_MONOTONIC, &start_compute);
             close(pfd[0]);
-            printf("Child (%d): Start the computation ...\n", (int)getpid());
+            printf("Child(%d) :Start the computation ...\n", (int)getpid());
             MSG *child = malloc(rows*sizeof(MSG));
             if (child == NULL) {
                     printf("Out of memory, can't create child MSG struct array!!\n");
@@ -68,7 +71,7 @@ int main( int argc, char* args[] )
             }
             int st_row_count = 0; //this count is for array of struct message
             // NEED TO FIND A BETTER WAY TO DIVIDE BLOCKS FOR COMPUTATION
-            printf("st_row_count1 = %d\n", st_row_count);
+            //printf("st_row_count1 = %d\n", st_row_count);
             for (int y=vert; y<rows+vert; y++) {
             //printf("Child(%d): y value %d and st_row_count is %d\n",(int)getpid(), y,st_row_count);
             if (y >= IMAGE_HEIGHT){
@@ -83,13 +86,16 @@ int main( int argc, char* args[] )
     	    }
             st_row_count += 1;
             }
-            printf("st_row_count2 = %d \n", st_row_count);
+            //printf("st_row_count2 = %d \n", st_row_count);
             for (int i = 0; i < st_row_count; i++){
                 write(pfd[1], &child[i], sizeof(MSG));
             }
-            printf("Reached here with PID %d \n", (int) getpid());
+            //printf("Reached here with PID %d \n", (int) getpid());
             st_row_count = 0;
             //close(pfd[1]);
+            clock_gettime(CLOCK_MONOTONIC, &end_compute);
+	        difftime = (end_compute.tv_nsec - start_compute.tv_nsec)/1000000.0 + (end_compute.tv_sec - start_compute.tv_sec)*1000.0;
+	        printf("Child(%d) :     ... completed. Elapse time = %.3f ms\n", (int)getpid(),difftime);
             exit(0);
             }
         vert += rows; 
@@ -105,16 +111,6 @@ int main( int argc, char* args[] )
             if (response == 0){
                 break;
             }
-            /*
-            int cur = receive.row_index;
-            if (prev != cur){
-                prev = cur;
-            }
-            else{
-                break;
-            }
-            */
-            //printf("received row value %d\n", receive.row_index);
             if (receive.row_index >= 0 && receive.row_index <= IMAGE_HEIGHT){
                 for(int j=0; j<IMAGE_WIDTH; j++){
                 //printf("rowdata for row_y %d is received as %.2f\n", receive.row_index , receive.rowdata[j]);
@@ -124,18 +120,26 @@ int main( int argc, char* args[] )
             
         }
         pid_t terminated_pid = wait(NULL);
-        printf("Child (%d) was terminated\n", (int) terminated_pid); 
         count += 1;
     }
+    printf("All Child processes have completed\n");
+    struct rusage usg, pusg;
+    if ( getrusage(RUSAGE_CHILDREN, &usg) == 0){
+        
+        printf("Total time spent by all child processes in user mode = %.3f ms\n", (double) (1000*(usg.ru_utime.tv_sec) + (usg.ru_utime.tv_usec)/1000) );
+        printf("Total time spent by all child processes in system mode = %.3f ms\n", (double) (1000*(usg.ru_stime.tv_sec) + (usg.ru_stime.tv_usec)/1000) );
 
-	clock_gettime(CLOCK_MONOTONIC, &end_compute);
-	difftime = (end_compute.tv_nsec - start_compute.tv_nsec)/1000000.0 + (end_compute.tv_sec - start_compute.tv_sec)*1000.0;
-	printf(" ... completed. Elapse time = %.3f ms\n", difftime);
+    }
+    if (getrusage (RUSAGE_SELF, &pusg) == 0){
+        printf("Total time spent by parent process in user mode = %.3f ms\n", (double) (1000*(pusg.ru_utime.tv_sec) + (pusg.ru_utime.tv_usec)/1000));
+        printf("Total time spent by parent process in system mode = %.3f ms\n", (double) (1000*(pusg.ru_stime.tv_sec) + (pusg.ru_stime.tv_usec)/1000));
+
+    }
 
 	//Report timing
 	clock_gettime(CLOCK_MONOTONIC, &end_time);
 	difftime = (end_time.tv_nsec - start_time.tv_nsec)/1000000.0 + (end_time.tv_sec - start_time.tv_sec)*1000.0;
-	printf("Total elapse time measured by the process = %.3f ms\n", difftime);
+	printf("Total elapse time measured by the parent process = %.3f ms\n", difftime);
 	
 	printf("Draw the image\n");
 	//Draw the image by using the SDL2 library
