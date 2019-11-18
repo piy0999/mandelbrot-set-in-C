62715c25fa2d2f7a639b2a331bf4a2e336f2fb4a
piyushjha <piyushjha0999@gmail.com>
2019-11-18 19:22:05 +0800

    add comments & FINAL

--- part2-mandelbrot.c
+++ part2-mandelbrot.c
@@ -2,7 +2,7 @@
 * Filename: part2-mandelbrot.c 
 * Student name: Piyush Jha
 * Student no.: 3035342691
-* Date: Nov 13, 2019
+* Date: Nov 18, 2019
 * version: 1.1
 * Development platform: Ubuntu 18.04 Cloud VM
 * Compilation: gcc part2-mandelbrot.c -o part2-mandelbrot -l SDL2 -l m -lpthread 
@@ -30,15 +30,19 @@ typedef struct task {
     int num_of_rows;
     int terminate;
 } TASK;
- 
+
+// Array of struct TASK which acts as the buffer for Boss Worker Interaction 
 TASK task_buffer[10];
+// Declare mutex for thread synchronisation
 pthread_mutex_t mv;
+// Declare condition Variables to check if the buffer is full or empty
 pthread_cond_t notFULL, notEMPTY;
+// initialise buffer count and total number of buffers
 int buff_count = 0;
 int num_buffers;
 
 
-//Arrays to store the PIDs and the number of times these processes work on tasks
+//Array to store the number of tasks processed by each worker
 int task_count[16];
 
 //generate mandelbrot image and store each pixel for later display
@@ -46,23 +50,34 @@ int task_count[16];
 //store the 2D image as a linear array of pixels (in row-major format)
 float * pixels;
 
-
+// Declare consumer function for each worker thread
 void *consumer (void *thdata) {
+    // get thread ID
     int id = *((int *) thdata);
+    // Loop until receive termination tasks
     while (1){
+    // Enable Mutex Lock
     pthread_mutex_lock(&mv); 
+    // Check if the buffer is not empty
     while (buff_count == 0){
+        // wait for TASK to be added to the empty buffer pool
         pthread_cond_wait(&notEMPTY, &mv);
     }
+    // Buffer is definitely not empty now so get a TASK from buffer
     TASK received;
     received = task_buffer[0];
+    // Remove the task from the TASK buffer
     for(int i=0; i<buff_count-1; i++)
     {
         task_buffer[i] = task_buffer[i + 1];
     }
+    // Reduce the counter for the buffer after removing the TASK
     buff_count -= 1;
+    // Signal waiting threads as buffer is not full
     pthread_cond_signal(&notFULL);
+    // release the mutex lock
     pthread_mutex_unlock(&mv);
+    // Exit the thread process if received a terminating TASK
     if (received.terminate == 1){
         break;
     }
@@ -88,6 +103,7 @@ void *consumer (void *thdata) {
     clock_gettime(CLOCK_MONOTONIC, &end_compute);
 	float difftime = (end_compute.tv_nsec - start_compute.tv_nsec)/1000000.0 + (end_compute.tv_sec - start_compute.tv_sec)*1000.0;
 	printf("Worker(%d) :     ... completed. Elapse time = %.3f ms\n", id,difftime);
+    // Increment the number of tasks performed against this thread ID
     task_count[id] += 1;
     }
     return NULL;
@@ -101,7 +117,7 @@ int main( int argc, char* args[] )
         printf("Usage: ./part2-mandelbrot [number of workers] [number of rows in a task] [number of buffers]");
         return 0;
     }
-    // store the number of threads from the arguments array
+    // store the number of worker threads from the arguments array
     char *a = args[1];
     int num_threads = atoi(a);
     // store the number of rows in a task from the arguments array
@@ -122,14 +138,12 @@ int main( int argc, char* args[] )
 	exit(1);
     }
 
-   
     float difftime;
-
-    
+       
     pthread_t * worker_thread = (pthread_t *) malloc(num_threads * sizeof(pthread_t));
-    
+    // Store worker thread IDs in array
     int worker_IDs[num_threads];
-	
+	// Create worker threads and call the consumer function using each thread
     for (int i = 0; i < num_threads; i++) {
 	worker_IDs[i] = i;
     task_count[i] = 0;
@@ -138,24 +152,33 @@ int main( int argc, char* args[] )
     // Counting the row of tasks distributed, the start row value for each task
     int r_count = 0;
 
+    // Loop until all tasks have been put into the TASK buffer
     while (r_count < IMAGE_HEIGHT){
+        // Create a TASK to be sent to TASK Buffer pool
         TASK send;
         send.num_of_rows = num_rows;
         send.start_row = r_count;
         send.terminate = 0;
         r_count += num_rows;
+        // Enable the mutex lock
         pthread_mutex_lock(&mv); 
-        
+        // Check if the current count of elements in buffer is equal to total number of buffers
         while (buff_count == num_buffers){
+            // Wait for TASK to be consumed from the Full Buffer pool
             pthread_cond_wait(&notFULL, &mv);
         }
+        // Add TASK to the Buffer Pool as the Buffer pool is definitely not full
         task_buffer[buff_count] = send;
-        
+        // Increment the buffer_counter as one more task is added to the Buffer Pool
         buff_count += 1;
+        // Signal waiting threads that the Buffer is not empty
         pthread_cond_signal(&notEMPTY);
+        // Release the Mutex Lock
         pthread_mutex_unlock(&mv);
         }
     
+    // Loop until the total number of working threads to add terminating task as all Tasks have been already sent into the TASK buffer
+    // Uses the same mechanism as described in the loop above only changes the send.terminate variable to 1 to indicate terminating TASK for the worker thread
     for (int i = 0; i < num_threads; i++) {
 	    TASK send;
         send.num_of_rows = 0;
@@ -172,7 +195,7 @@ int main( int argc, char* args[] )
         pthread_mutex_unlock(&mv);
     }
 
-
+    // Wait for all the worker threads to be terminated
     for (int i = 0; i < num_threads; i++) {
 	    pthread_join(worker_thread[i], NULL);
         printf("Worker thread %d has terminated and completed %d tasks\n", i, task_count[i]);
@@ -181,7 +204,7 @@ int main( int argc, char* args[] )
     
     printf("All worker threads have terminated\n");
     
-    // Using getrusage to display the statistics of the runtimes first for the child processes and then the parent process
+    // Using getrusage to display the statistics of the runtimes for the master process and the worker threads
     struct rusage pusg;
     if (getrusage (RUSAGE_SELF, &pusg) == 0){
         printf("Total time spent by process and its threads in user mode = %.3f ms\n", (double) (1000*(pusg.ru_utime.tv_sec) + (pusg.ru_utime.tv_usec)/1000));
